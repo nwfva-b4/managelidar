a57fc95768574e7f3ab5714f0570642dd122dc03
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2024-07-16 12:17:36 +0200

    update naming management

--- R/check_names.R
+++ R/check_names.R
@@ -1,76 +1,64 @@
-
 #' Check file names
 #'
-#' Checks the file names according to our own standard.
+#' Checks the file names according to [ADV standard](https://www.adv-online.de/AdV-Produkte/Standards-und-Produktblaetter/Standards-der-Geotopographie/binarywriterservlet?imgUid=6b510f6e-a708-d081-505a-20954cd298e1&uBasVariant=11111111-1111-1111-1111-111111111111).
 #' File names should be in the following schema:
-#' "prefix_UTMzone_minx_miny_tilesize_region_acquisitiondate.laz"
+#' `prefix`_`utmzone`_`minx`_`miny`_`tilesize`_`region`_`year``.laz`
 #'
-#' (e.g. "3dm_32_547_5724_1_ni_20240327.laz")
+#' (e.g. `3dm_32_547_5724_1_ni_2024.laz`)
 #'
-#' @param path A path to a laz file or a directory which contains laz files
+#' @param path A path to a directory which contains las/laz files
 #' @param prefix 3 letter character. Naming prefix (defaults to "3dm")
 #' @param zone 2 digits integer. UTM zone (defaults to 32)
 #' @param region 2 letter character. Region abbreviation (defaults to "ni")
-#' @param date YYYYMMDD. (optional) acquisition date
-#' @param full.names Whether to return the full file path or just the file name (default)
+#' @param year YYYY. (optional) acquisition year to append to filename. 
+#' If not provided the year will be extracted from the files. It will be the acquisition date if points contain datetime in GPStime format, otherwise it will get the year from the file header, which is the processing date by definition.  
+#' @param full.names Whether to return the full file path or just the file names (default)
 #'
 #' @return A dataframe with name_is, name_should, correct
 #' @export
 #'
 #' @examples
-#' f <- system.file("extdata", package="managelidar")
+#' f <- system.file("extdata", package = "managelidar")
 #' check_names(f)
-check_names <- function(path, prefix = "3dm", zone = 32, region = "ni", date = NULL, full.names = FALSE){
+check_names <- function(path, prefix = "3dm", zone = 32, region = "ni", year = NULL, full.names = FALSE) {
+  print("create VPC with GPStime")
+  ans <- lasR::exec(
+    lasR::set_crs(25832) + lasR::write_vpc(ofile = tempfile(fileext = ".vpc"), use_gpstime = TRUE, absolute_path = TRUE),
+    with = list(ncores = lasR::concurrent_files(lasR::half_cores())),
+    on = path
+  )
 
-  check_file_names <- function(file){
-
-    if(is.null(date)){
-      date = substr(basename(file), 22, 29)
-    }
-
-    fileheader <- lidR::readLASheader(file)
-    minx = floor(fileheader$`Min X` / 1000)
-    miny = floor(fileheader$`Min Y` / 1000)
-    maxx = ceiling(fileheader$`Max X` / 1000)
-    maxy = ceiling(fileheader$`Max Y` / 1000)
-
-    if((maxx - minx) == (maxy - miny)){
-      tilesize = maxx - minx
-    } else
-      {print("tiles are not quadratic")}
+  json <- jsonlite::fromJSON(ans)
 
 
+  bbox <- json$features$properties$`proj:bbox`
 
-    name_should = paste0(prefix, "_", zone, "_", minx, "_", miny, "_", tilesize, "_", region, "_", date, ".laz")
+  minx <- sapply(bbox, function(x) floor(round(x[1] / 1000, 2)))
+  miny <- sapply(bbox, function(x) floor(round(x[2] / 1000, 2)))
+  maxx <- sapply(bbox, function(x) floor(round(x[3] / 1000, 2)))
 
-    if(basename(file) != name_should){
-      correct = FALSE
-    }
-    else{
-      correct = TRUE
-    }
-
-    if (full.names == FALSE){
-      file <- basename(file)
-    }
-    if (full.names == TRUE){
-      name_should <- file.path(dirname(file), name_should)
-    }
+  tilesize <- maxx - minx
 
 
-    return(data.frame(name_is = file,
-                      name_should = name_should,
-                      correct_naming = correct))
-
+  if (is.null(year)) {
+    # set year to acqusition /processing year
+    year <- format(as.Date(json$features$properties$datetime), "%Y")
   }
 
-  if (file.exists(path) && !dir.exists(path)) {
-
-    check_file_names(path)
+  if (full.names == FALSE) {
+    name_is <- basename(json$features$asset$data$href)
+    name_should <- paste0(prefix, "_", zone, "_", minx, "_", miny, "_", tilesize, "_", region, "_", year, ".", tools::file_ext(json$features$asset$data$href))
+  } else {
+    name_is <- json$features$asset$data$href
+    name_should <- file.path(dirname(name_is), paste0(prefix, "_", zone, "_", minx, "_", miny, "_", tilesize, "_", region, "_", year, ".", tools::file_ext(json$features$asset$data$href)))
   }
-  else {
-    files <- list.files(path, pattern = "*.laz$", full.names = T)
-    return(as.data.frame(do.call(rbind, lapply(files, check_file_names))))
-  }
 
+
+  dat <- data.frame(
+    name_is = name_is,
+    name_should = name_should,
+    correct = name_is == name_should
+  )
+
+  return(dat)
 }
--- R/set_names.R
+++ R/set_names.R
@@ -1,29 +1,30 @@
-
-
 #' Set file names
 #'
-#' Renames files according to schema validated by `check_files()`.
+#' Renames files according to schema validated by `check_files()`. This is according to [ADV standard](https://www.adv-online.de/AdV-Produkte/Standards-und-Produktblaetter/Standards-der-Geotopographie/binarywriterservlet?imgUid=6b510f6e-a708-d081-505a-20954cd298e1&uBasVariant=11111111-1111-1111-1111-111111111111).
 #'
-#' @param path Path to folder containing laz files
+#' @param path A path to a directory which contains las/laz files
+#' @param prefix 3 letter character. Naming prefix (defaults to "3dm")
+#' @param zone 2 digits integer. UTM zone (defaults to 32)
+#' @param region 2 letter character. Region abbreviation (defaults to "ni")
+#' @param year YYYY. (optional) acquisition year to append to filename. 
+#' If not provided the year will be extracted from the files. It will be the acquisition date if points contain datetime in GPStime format, otherwise it will get the year from the file header, which is the processing date by definition.  
 #'
 #' @return Renamed files
 #' @export
 #'
 #' @examples
-#' f <- system.file("extdata/3dm_32_547_5724_1_ni_20240327.laz", package="managelidar")
-#' tmpname <- file.path(dirname(f), "wrongname.laz")
-#' file.copy(f, tmpname)
-#' set_names(tmpname)
-#' file.remove(tmpname)
-set_names <- function(path, prefix = "3dm", zone = 32, region = "ni", date = NULL){
-  t <- managelidar::check_names(path, prefix, zone, region, date, full.names = T)
-  t <- subset(t, correct_naming == FALSE)
+#' f <- system.file("extdata/3dm_32_547_5724_1_ni_20240327.laz", package = "managelidar")
+#' copy <- tempfile(fileext = ".laz")
+#' file.copy(f, copy)
+#' set_names(copy)
+set_names <- function(path, prefix = "3dm", zone = 32, region = "ni", year = NULL) {
+  t <- managelidar::check_names(path, prefix, zone, region, year, full.names = T)
+  t <- subset(t, correct == FALSE)
 
-  if(ncol(t) == 0L){
+  if (ncol(t) == 0L) {
     stop("all names already as expected")
   }
 
-  print(t)
   print(paste0("Renaming ", nrow(t), " files"))
 
   file.rename(from = t$name_is, to = t$name_should)
