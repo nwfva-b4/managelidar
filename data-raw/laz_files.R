fab58d104753a284a1775c606fe1fcd9fdff85e0
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2024-11-27 12:39:58 +0100

    fix typo

--- data-raw/laz_files.R
+++ data-raw/laz_files.R
@@ -1,5 +1,5 @@
 
-# get sample data from ALS campaign "Solling 2024" and downsample to 2m to reduce file size
+# get sample data from ALS campaign "Solling 2024" and downsample to 10m to reduce file size
 
 path <- "L:/luftbilder/aktuell/2024/Solling_20240327/Laserscan/klassifizierte_Laserdaten/"
 f <- c(paste0(path, "3dm_32_547_5724_1_ni_20240327.laz"),
