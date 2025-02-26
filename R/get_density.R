11b010f69f4ccfb85c029e5bc584622e7b6c276e
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2025-02-26 17:26:59 +0100

    add point density

--- R/get_density.R
+++ R/get_density.R
@@ -1,15 +1,15 @@
 #' Get the approximate pulse density of lasfiles
 #'
-#' `get_density()` calculates the approximate average pulse density (first/last-return only) of lasfiles.
+#' `get_density()` calculates the approximate average point and pulse (first/last-return only) density of lasfiles.
 #'
-#' For this function only the header from lasfiles is read and density is calculated from the bounding box of the data file and the number of first-returns. This does not take into account if parts of the bounding box is missing data, and hence this density does not reflect the density as it is calculates by e.g. `lidR`. However, it is much faster because it does not read the entire file and density should be approximately the same if the entire bounding box has point data.
+#' For this function only the header from lasfiles is read and density is calculated from the bounding box of the data file and the number of points or first-returns. This does not take into account if parts of the bounding box is missing data, and hence this density does not reflect the density as it is calculates by e.g. `lidR`. However, it is much faster because it does not read the entire file and density should be approximately the same if the entire bounding box has point data.
 #'
 #'
 #' @param path A path to a laz file or a directory which contains laz files
 #'
 #' @param full.names Whether to return the full file path or just the file name (default)
 #'
-#' @return A dataframe with file, pulses, area, density
+#' @return A dataframe with file, points, pulses, area, pointdensity, pulsedensity
 #' @export
 #'
 #' @examples
@@ -25,16 +25,18 @@ get_density <- function(path, full.names = FALSE){
     maxx = fileheader$`Max X`
     maxy = fileheader$`Max Y`
 
-    firstreturns = fileheader$`Number of points by return`[1]
+    points = fileheader$`Number of point records`
+    pulses = fileheader$`Number of points by return`[1]
     bbox = sf::st_bbox(c(xmin = minx, xmax = maxx, ymax = maxy, ymin = miny), crs = sf::st_crs(fileheader))
     area = sf::st_area(sf::st_as_sfc(bbox))
-    density =  firstreturns / area
+    pointdensity = points / area
+    pulsedensity =  pulses / area
 
     if (full.names == FALSE){
       file <- basename(file)
     }
 
-    return(data.frame(file = file, pulses = firstreturns, area = area, density = density))
+    return(data.frame(file = file, points = points, pulses = pulses, area = area, pointdensity = pointdensity, pulsedensity = pulsedensity))
 
   }
 
