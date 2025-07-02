b97862783796f38f0190078df66739d4c9f3562f
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2025-07-02 15:05:58 +0200

    update function to work with vpc

--- R/get_penetration.R
+++ R/get_penetration.R
@@ -1,15 +1,14 @@
-#' Get the pulse penetration ratio of lasfiles
+#' Get the pulse penetration ratio of LAS files
 #'
-#' `get_penetration()` calculates the approximate pulse penetration ratio of lasfiles.
+#' `get_penetration()` calculates the approximate pulse penetration ratio of LAS files.
 #'
 #' For this function only the header from lasfiles is read. It calculates the ratio of pulses which have a single return only, which have two returns, three returns, four returns, five returns, six returns and for convenience multiple returns (two or more). Beware that this does not have to be exact especially for small files since pulses can be split at borders.
 #'
 #'
-#' @param path A path to a laz file or a directory which contains laz files
+#' @param path The path to a LAS file (.las/.laz/.copc), to a directory which contains LAS files, or to a Virtual Point Cloud (.vpc) referencing LAS files.
+#' @param full.names Whether to return the full file paths or just the filenames (default) Whether to return the full file path or just the file name (default)
 #'
-#' @param full.names Whether to return the full file path or just the file name (default)
-#'
-#' @return A dataframe with file, single, two, three, four, five, six, multiple
+#' @return A data.frame with attributes `filename`, `single`, `two`, `three`, `four`, `five`, `six` and `multiple`
 #' @export
 #'
 #' @examples
@@ -41,15 +40,32 @@ get_penetration <- function(path, full.names = FALSE){
       file <- basename(file)
     }
 
-    return(data.frame(file = file, single = round(singleton, 3), two = round(doubleton, 3), three = round(triple, 3), four = round(quadruple, 3), five = round(quintuple, 3), six = round(sextuple, 3), multiple = round(multiton, 3)))
+    return(data.frame(filename = file, single = round(singleton, 3), two = round(doubleton, 3), three = round(triple, 3), four = round(quadruple, 3), five = round(quintuple, 3), six = round(sextuple, 3), multiple = round(multiton, 3)))
 
   }
 
-  if (file.exists(path) && !dir.exists(path)) {
-    return(as.data.frame(get_file_penetration(path)))
+if (file.exists(path) && !dir.exists(path)) {
+
+    # Virtual Point Cloud
+    if (tools::file_ext(path) == "vpc") {
+      vpc <- yyjsonr::read_json_file(path)
+      f <- sapply(vpc$features$assets, function(x) x$data$href)
+      return(as.data.frame(do.call(rbind, lapply(f, get_file_penetration))))
+    }
+    # LAZ file
+    else if (tools::file_ext(path) %in% c("las", "laz")) {
+      return(as.data.frame(get_file_penetration(path)))
+    } else {
+      stop("Unsupported file format. Supported formats: .las, .laz, .vpc")
+    }
   }
-  else {
-    f <- list.files(path, pattern = "*.laz$", full.names = TRUE)
+
+  # Folder Path
+  else if (dir.exists(path)) {
+
+    f <- list.files(path, pattern = "\\.(las|laz)$", full.names = TRUE)
     return(as.data.frame(do.call(rbind, lapply(f, get_file_penetration))))
+  } else {
+    stop("Path does not exist: ", path)
   }
 }
