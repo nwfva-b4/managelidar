ae6a423d507d5509e570fb9ba21441e50df573d7
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2025-07-02 15:01:41 +0200

    rewrite to use information from vpc

--- R/get_extent.R
+++ R/get_extent.R
@@ -1,42 +1,61 @@
-#' Get the spatial extent of laz files
+#' Get the spatial extent of LAS files
 #'
 #' `get_extent` uses min and max values of spatial extent defined in the header of lasfiles.
 #'
-#' @param path Either a path to a directory which contains laz files or
-#' the path to a Virtual Point Cloud (.vpc) created with lasR package.
+#' @param path path The path to a file (.las/.laz/.copc), to a directory which contains these files, or to a virtual point cloud (.vpc) referencing these files.
+#' @param full.names Whether to return the full file paths or just the filenames (default) Whether to return the full file path or just the file name (default)
+#' @param as_sf Whether to return the dataframe as spatial features
 #'
-#' @param full.names Whether to return the full file path or just the file name (default)
-#'
-#' @return A dataframe with file, minx, miny, minz, maxx, maxy, maxz
+#' @return A data.frame with attributes `filename`, `xmin`, `xmax`, `ymin` and `ymax`
 #' @export
 #'
 #' @examples
-#' f <- system.file("extdata", package="managelidar")
+#' f <- system.file("extdata", package = "managelidar")
 #' get_extent(f)
-get_extent <- function(path, full.names = FALSE){
+get_extent <- function(path, as_sf = FALSE, full.names = FALSE) {
 
-  get_file_extent <- function(file){
-    fileheader <- lidR::readLASheader(file)
+  # Virtual Point Cloud
+  if (tools::file_ext(path) == "vpc") {
+    print("Using information stored in Virtual Point Cloud")
+    ans <- path
+  } else if (
+  # LAZ file
+  tools::file_ext(path) %in% c("las", "laz")) {
+    print("Using file to build temporary Virtual Point Cloud")
+    ans <- lasR::exec(lasR::write_vpc(tempfile(fileext = ".vpc"), absolute_path =TRUE), on = path)
+  } else if (
 
-    minx = fileheader$`Min X`
-    miny = fileheader$`Min Y`
-    minz = fileheader$`Min Z`
-    maxx = fileheader$`Max X`
-    maxy = fileheader$`Max Y`
-    maxz = fileheader$`Max Z`
-
-    if (full.names == FALSE){
-      file <- basename(file)
-    }
-
-    return(data.frame(file = file, minx, miny, minz, maxx, maxy, maxz))
-
-  }
-
-  if (file.exists(path) && !dir.exists(path)) {
-    return(as.data.frame(get_file_extent(path)))
+  # Folder Path
+  dir.exists(path)) {
+    print("Using directory to build temporary Virtual Point Cloud")
+    ans <- lasR::exec(lasR::write_vpc(tempfile(fileext = ".vpc"), absolute_path =TRUE), on = path)
   } else {
-    f <- list.files(path, pattern = "*.laz$", full.names = TRUE)
-    return(as.data.frame(do.call(rbind, lapply(f, get_file_extent))))
+    stop("Path does not exist: ", path, "Use either existing Virtual Point Cloud (.vpc), LAS file, or folder which contains LAS files.")
   }
+
+  # read info from vpc
+  vpc <- yyjsonr::read_json_file(ans)
+  t <- data.frame(filename = sapply(vpc$features$assets, function(x) x$data$href),
+                  xmin = sapply(vpc$features$properties, function(x) x$`proj:bbox`[[1]]),
+                  xmax = sapply(vpc$features$properties, function(x) x$`proj:bbox`[[3]]),
+                  ymin = sapply(vpc$features$properties, function(x) x$`proj:bbox`[[2]]),
+                  ymax = sapply(vpc$features$properties, function(x) x$`proj:bbox`[[4]])
+  )
+
+
+
+  # optionally as spatial features
+  if (as_sf) {
+    t <- sf::st_sf(t, geometry = sf::st_read(ans)$geometry)
+    t <- sf::st_zm(t)
+  }
+
+  # return basename or full name
+  if (full.names == FALSE){
+    t$filename <- basename(t$filename)
+  }
+
+  return(t)
 }
+
+
