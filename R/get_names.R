04632b542e70422a6ea65d19b7df2e3ae894dddd
Jens Wiesehahn <wiesehahn.jens@gmail.com>
2025-09-08 08:55:56 +0200

    update documentation

--- R/get_names.R
+++ R/get_names.R
@@ -1,6 +1,6 @@
 #' Get file names
 #'
-#' Simply get a vector of names of all LAS files (*.las, *.laz, *.laz.copc) in the folder.
+#' Simply get a vector of names of all LAS files (*.las, *.laz, *.laz.copc) in a folder or VPC.
 #'
 #' @param path A path to a LAS file, VPC file, or a directory which contains LAS files
 #'
@@ -13,10 +13,10 @@
 #' f <- system.file("extdata", package = "managelidar")
 #' get_names(f)
 get_names <- function(path, full.names = FALSE) {
+  
   # Single file input
-
   if (file.exists(path) && !dir.exists(path)) {
-    # Single file input
+    # Virtual Point Cloud
     if (tools::file_ext(path) == "vpc") {
       t <- yyjsonr::read_json_file(path)
       files <- sapply(t$features$assets, function(x) x$data$href)
@@ -25,6 +25,7 @@ get_names <- function(path, full.names = FALSE) {
       } else {
         file <- files
       }
+    # LAS/LAZ/COPC file
     } else if (tools::file_ext(path) %in% c("las", "laz", "copc")) {
       if (full.names == FALSE) {
         file <- basename(path)
@@ -35,7 +36,7 @@ get_names <- function(path, full.names = FALSE) {
       stop("Unsupported file format. Supported formats: .las, .laz, .laz.copc, .vpc")
     }
   } else {
-    # Pattern to match .las, .laz, and .laz.copc files
+    # Folder path
     file <- list.files(path, pattern = "\\.(las|laz(\\.copc)?)$", full.names = full.names)
   }
 
